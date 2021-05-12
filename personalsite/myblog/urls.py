567ff1b6f11a79a57ed976863e355547fdaaaaa4
luisjwhitaker <luisjwhitaker@gmail.com>
2021-05-12 13:36:15 -0700

    added postpage and made post previews route to it.

--- personalsite/myblog/urls.py
+++ personalsite/myblog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_home, name='blog home'),
+    path('<str:primery_key>/', views.post_page, name='post page'),
 ]
--- personalsite/myblog/views.py
+++ personalsite/myblog/views.py
@@ -3,6 +3,10 @@ from .models import *
 
 # Create your views here.
 
-def blog_home(request):
+def blog_home(request): 
     items = BlogPost.objects.all()
     return render(request,'myblog/bloghome.html',{'items':items})
+
+def post_page(request,primery_key):
+    post = BlogPost.objects.get(id=primery_key)
+    return render(request,'myblog/postpage.html',{'post':post})
