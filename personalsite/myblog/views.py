a1b75f6c3380808a1df0d67dcbd671b24a268a33
luisjwhitaker <luisjwhitaker@gmail.com>
2021-05-18 21:22:48 -0700

    added basic pagination to blogsite app.

--- personalsite/myblog/views.py
+++ personalsite/myblog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import *
-
+from django.core.paginator import Paginator
 # Create your views here.
 
-def blog_home(request): 
+def blog_home(request):
     items = BlogPost.objects.all()
-    return render(request,'myblog/bloghome.html',{'items':items})
+    paginator = Paginator(items, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request,'myblog/bloghome.html',{'items':items,'page_obj':page_obj})
 
 def post_page(request,primery_key):
     post = BlogPost.objects.get(id=primery_key)
