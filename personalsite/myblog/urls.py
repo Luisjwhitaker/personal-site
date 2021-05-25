220070378b53c61972dae8eabb74bfd4387b7529
luisjwhitaker <luisjwhitaker@gmail.com>
2021-05-25 05:23:38 -0700

    fixed the 'cannot upload image with summernote' issue.

--- personalsite/myblog/urls.py
+++ personalsite/myblog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_home, name='blog home'),
+    path('', views.portfolio_home, name='portfolio home'),
+    path('blog/', views.blog_home, name='blog home'),
     path('<str:primery_key>/', views.post_page, name='post page'),
 ]
--- personalsite/myblog/views.py
+++ personalsite/myblog/views.py
@@ -5,13 +5,17 @@ from django.core.paginator import Paginator
 
 def blog_home(request):
     items = BlogPost.objects.all()
-    paginator = Paginator(items, 3)
+    paginator_object = Paginator(items, 5)
+    # paginator class takes list of objects and number of objects you want form the list per page and
 
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator_object.get_page(page_number)
 
     return render(request,'myblog/bloghome.html',{'items':items,'page_obj':page_obj})
 
 def post_page(request,primery_key):
     post = BlogPost.objects.get(id=primery_key)
     return render(request,'myblog/postpage.html',{'post':post})
+
+def portfolio_home(request):
+    return render(request,'myblog/portfolio.html')
