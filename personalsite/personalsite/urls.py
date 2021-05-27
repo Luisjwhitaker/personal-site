b199627881d5e58639f344220d44e37b656bcd35
luisjwhitaker <luisjwhitaker@gmail.com>
2021-05-27 07:29:19 -0700

    Added coffeecat app to personal-site project.

--- personalsite/personalsite/urls.py
+++ personalsite/personalsite/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myblog.urls')),
+    path('coffe/', include('coffeecat.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
 if settings.DEBUG:
