67e90d8d18e083f07135f8168a7c1d2d47fa9ee6
user.email <luisjwhitaker@gmail.com>
2022-02-24 23:17:50 -0800

    fixed environment variable typo

--- personalsite/personalsite/settings.py
+++ personalsite/personalsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-fe2tr^g^!zknrk*$agiptr%jjb_maqpy5j%-w1#344xq32knj!'
-SECRET_KEY = os.environment.get('PERSONAL_SITE_SECRET_KEY')
+SECRET_KEY = os.environ.get('PERSONAL_SITE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
