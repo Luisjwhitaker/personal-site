8ffb744e12674f919a99bd5cb246a0a9252296d3
user.email <luisjwhitaker@gmail.com>
2022-02-27 16:20:14 -0800

    Set Debug Value

--- personalsite/personalsite/settings.py
+++ personalsite/personalsite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('PERSONAL_SITE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('PERSONAL_SITE_DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['luisjohnwhitakerwebsite.herokuapp.com']
 
