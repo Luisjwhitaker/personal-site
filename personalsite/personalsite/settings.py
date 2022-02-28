5ab68de8406b602f3b50a596be526bbac807329e
user.email <luisjwhitaker@gmail.com>
2022-02-27 16:20:41 -0800

    Removed Old SecretKey

--- personalsite/personalsite/settings.py
+++ personalsite/personalsite/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-fe2tr^g^!zknrk*$agiptr%jjb_maqpy5j%-w1#344xq32knj!'
 SECRET_KEY = os.environ.get('PERSONAL_SITE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
